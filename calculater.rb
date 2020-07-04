c8a0482e8002d18c39408e61c7207c261c9f6980
parthbpatel <parthtech3010@gmail.com>
2020-07-04 16:00:39 +0530

    calculator in ruby

---
+++ calculater.rb
@@ -0,0 +1,25 @@
+def sum_of_two_number(number1, number2)
+  puts number1 + number2
+end
+
+def substraction_of_two_number(number3, number4)
+  puts number3 - number4
+end
+
+def multiplication_of_two_number(z, x)
+  puts z * x
+end
+
+def divide_of_two_number(a, s)
+  puts a / s
+end
+
+def modulo_of_two_numbers(h, i)
+  puts h % i
+end
+
+sum_of_two_number(9, 5)
+substraction_of_two_number(83, 45)
+multiplication_of_two_number(45, 5)
+divide_of_two_number(65, 5)
+modulo_of_two_numbers(7, 4)
